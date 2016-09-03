570bc2cc928b22ea8a07fbdc76a38e2f7077643d
Pezhman  Jahanmard <pezhman32@gmail.com>
2016-09-03 13:40:05 +0430

    init

---
+++ src/main/java/com/sinan/test/SpringInMemoryDbWithRestApplication.java
@@ -0,0 +1,12 @@
+package com.sinan.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInMemoryDbWithRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInMemoryDbWithRestApplication.class, args);
+	}
+}
